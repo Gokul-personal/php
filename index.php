158b0559cd9bbb87a77722b70ef45b0e5a1b78f4
Gokul-personal <145566281+Gokul-personal@users.noreply.github.com>
2023-12-01 13:00:58 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Azure PHP App</title>
+</head>
+<body>
+    <h1>Hello Azure Web App!</h1>
+    <p>This is a simple PHP web application deployed on Azure.</p>
+</body>
+</html>
