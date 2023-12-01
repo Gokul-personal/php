620f2dbaca33af501184ee98d8c97383f3f4b51b
Vigneshp30 <145566281+Gokul-personal@users.noreply.github.com>
2023-12-01 14:06:31 +0530

    s

--- index.php
+++ index.php
@@ -1,1 +1,313 @@
-welcome
+
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <meta name="keywords" content="">
+  <meta name="description" content="">
+  <title>Farm Management</title>
+  <!--Bootstrap -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/style.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/owl.carousel.css" type="text/css">
+  <link rel="stylesheet" href="assets/css/owl.transitions.css" type="text/css">
+  <link href="assets/css/slick.css" rel="stylesheet">
+  <link href="assets/css/bootstrap-slider.min.css" rel="stylesheet">
+  <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+  <!-- Fav and touch icons -->
+  <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/favicon-icon/logo.png">
+  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon-icon/logo.png">
+  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon-icon/logo.png">
+  <link rel="apple-touch-icon-precomposed" href="assets/images/favicon-icon/logo.png">
+  <link rel="shortcut icon" href="assets/images/favicon-icon/logo.png">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+ <!--FontAwesome Font Style -->
+ <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <!--Header-->
+  <?php include('includes/header.php'); ?>
+  <!-- /Header -->
+
+  <!-- Banners -->
+  <section id="banner" class="banner-section">
+    <div class="container">
+      <div class="div_zindex">
+        <div class="row">
+          <div class="col-md-5 col-md-push-7">
+            <div class="banner_content">
+              <h1>Learn the farm details</h1>
+              <p>Without agriculture it is not possible to have a city, stock market, banks, university, church or army </p>
+              <a href="#" class="btn">Read More <span class="angle_arrow"><i class="fa fa-angle-right" aria-hidden="true"></i></span></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- /Banners -->
+  <!-- Resent Cat-->
+  <section class="section-padding gray-bg">
+    <div class="container">
+      <div class="section-header text-center">
+
+     
+
+        <h2>Agriculture Introduction</h2>
+        <p>Agriculture is the broad term that encompasses the science, art, and business of
+          cultivating the soil, growing crops, and raising livestock. It is a multifaceted activity that
+          involves various practices, technologies, and principles aimed at producing food, fiber, and
+          other raw materials to meet the needs of human societies.
+
+          Farming and agriculture are the foundational pillars of human civilization, representing the
+          art and science of cultivating the land to produce food, fiber, and other resources essential
+          for sustenance.
+
+          Agriculture is not only a means of sustenance but also a way of life, deeply intertwined with
+          cultural practices and traditions. It has played a pivotal role in the rise of civilizations,
+          enabling the establishment of settled communities and fostering the growth of economies.
+
+          The advent of industrialization brought mechanization to agriculture, revolutionizing the
+          sector with machinery like tractors, combine harvesters, and irrigation systems, significantly
+          increasing efficiency. farming and agriculture represent a dynamic interplay between nature
+          and human ingenuity, providing the sustenance that fuels our societies while constantly
+          adapting to the ever-changing demands of a growing global population.
+
+          Crop farming involves the cultivation of plants for human consumption or industrial use.
+          Farmers select and grow specific crops, such as grains, fruits, vegetables, and legumes,
+          depending on climate, soil conditions, and market demand. The process includes preparing
+          the soil, planting seeds or seedlings, applying fertilizers and pesticides, and harvesting the
+          mature crops.
+
+          Farming, often referred to as agriculture, is the practice of cultivating crops, raising
+          livestock, and managing other activities related to the production of food. Agriculture
+          involves a complex and interactive relationship between animals, plants, and humans. The
+          ways in which agriculture is practiced can have profound impacts on the environment and
+          natural ecosystems. Earthworms are known as farmers' best friends because they provide
+          multiple services that improve soil health and consequently plant health.</p>
+          <div class="wrapper">
+     
+      <div class="carousel">
+        <img src="image/ten/Agriculture1.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture2.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture3.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture4.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture5.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture6.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture7.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture8.jpg" alt="img" draggable="false">
+        <img src="image/ten/Agriculture9.jpg" alt="img" draggable="false">
+      </div>
+      <i id="right" class="fa-solid fa-angle-right"></i>
+    </div>
+    <br>
+    <br>
+        <h4>Traditional Farming</h4>
+        <p>It often involves manual labour, simple tools, and traditional crop varieties. Traditional
+          farming methods are often influenced by local climate, soil conditions, and cultural
+          practices. The use of organic fertilizers, crop rotation, and reliance on natural rainfall are
+          common features of traditional farming.</p>
+          <div class="wrapper">
+     
+     <div class="carousel">
+       <img src="image/Tradition/Traditionone.jpg" alt="img" draggable="false">
+       <img src="image/Tradition/Traditiontwo.jpg" alt="img" draggable="false">
+       <img src="image/Tradition/Traditionthree.jpg" alt="img" draggable="false">
+       
+     </div>
+  
+   </div>
+   <br>
+   <br>
+          
+        <h4>Modern Farming</h4>
+        <p>It incorporates the latest scientific and technological innovations, including machinery,
+          genetically modified crops, precision farming techniques, and artificial irrigation. Modern
+          farming aims for higher efficiency, increased crop yield, and sustainable practices. It often
+          involves the use of agrochemicals, such as fertilizers and pesticides, and relies on
+          sophisticated equipment for planting, harvesting, and irrigation.</p>
+    </div>
+
+     <div class="wrapper">
+     
+      <div class="carousel">
+        <img src="image/Modern/Modernone.jpg" alt="img" draggable="false">
+        <img src="image/Modern/Moderntwo.jpg" alt="img" draggable="false">
+        <img src="image/Modern/Modernthree.jpg" alt="img" draggable="false">
+        
+      
+    <br>
+  </section>
+
+  <!--Footer -->
+  <?php include('includes/footer.php'); ?>
+  <!-- /Footer-->
+
+  <!--Login-Form -->
+  <?php include('includes/login.php'); ?>
+  <!--/Login-Form -->
+
+  <!--Register-Form -->
+  <?php include('includes/registration.php'); ?>
+
+  <!--/Register-Form -->
+
+  <!--Forgot-password-Form -->
+  <?php include('includes/forgotpassword.php'); ?>
+  <!--/Forgot-password-Form -->
+
+  <!-- Scripts -->
+  <script src="assets/js/jquery.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+  <script src="assets/js/interface.js"></script>
+
+  <!--bootstrap-slider-JS-->
+  <script src="assets/js/bootstrap-slider.min.js"></script>
+  <!--Slider-JS-->
+  <script src="assets/js/slick.min.js"></script>
+  <script src="assets/js/owl.carousel.min.js"></script>
+
+
+<!--some of additional codes -->
+
+
+
+
+<style> 
+.wrapper{
+  display: flex;
+  max-width: 100%;
+  position: relative;
+}
+.wrapper i{
+  top: 50%;
+  height: 44px;
+  width: 44px;
+  color: #343F4F;
+  cursor: pointer;
+  font-size: 1.15rem;
+  position: absolute;
+  text-align: center;
+  line-height: 44px;
+  background: #fff;
+  border-radius: 50%;
+  transform: translateY(-50%);
+  transition: transform 0.1s linear;
+}
+.wrapper i:active{
+  transform: translateY(-50%) scale(0.9);
+}
+.wrapper i:hover{
+  background: #f2f2f2;
+}
+.wrapper i:first-child{
+  left: -22px;
+  display: ;
+}
+.wrapper i:last-child{
+  right: -22px;
+}
+.wrapper .carousel{
+  font-size: 0px;
+  cursor: pointer;
+  overflow: hidden;
+  white-space: nowrap;
+  scroll-behavior: smooth;
+}
+.carousel.dragging{
+  cursor: grab;
+  scroll-behavior: auto;
+}
+.carousel.dragging img{
+  pointer-events: none;
+}
+.carousel img{
+  height: 340px;
+  object-fit: cover;
+  user-select: none;
+  margin-left: 14px;
+  width: calc(100% / 3);
+}
+.carousel img:first-child{
+  margin-left: 0px;
+}
+
+
+</style>
+
+<script>
+  const carousel = document.querySelector(".carousel"),
+firstImg = carousel.querySelectorAll("img")[0],
+arrowIcons = document.querySelectorAll(".wrapper i");
+let isDragStart = false, isDragging = false, prevPageX, prevScrollLeft, positionDiff;
+const showHideIcons = () => {
+    // showing and hiding prev/next icon according to carousel scroll left value
+    let scrollWidth = carousel.scrollWidth - carousel.clientWidth; // getting max scrollable width
+    arrowIcons[0].style.display = carousel.scrollLeft == 0 ? "none" : "block";
+    arrowIcons[1].style.display = carousel.scrollLeft == scrollWidth ? "none" : "block";
+}
+arrowIcons.forEach(icon => {
+    icon.addEventListener("click", () => {
+        let firstImgWidth = firstImg.clientWidth + 14; // getting first img width & adding 14 margin value
+        // if clicked icon is left, reduce width value from the carousel scroll left else add to it
+        carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        setTimeout(() => showHideIcons(), 60); // calling showHideIcons after 60ms
+    });
+});
+const autoSlide = () => {
+    // if there is no image left to scroll then return from here
+    if(carousel.scrollLeft - (carousel.scrollWidth - carousel.clientWidth) > -1 || carousel.scrollLeft <= 0) return;
+    positionDiff = Math.abs(positionDiff); // making positionDiff value to positive
+    let firstImgWidth = firstImg.clientWidth + 14;
+    // getting difference value that needs to add or reduce from carousel left to take middle img center
+    let valDifference = firstImgWidth - positionDiff;
+    if(carousel.scrollLeft > prevScrollLeft) { // if user is scrolling to the right
+        return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+    }
+    // if user is scrolling to the left
+    carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
+}
+const dragStart = (e) => {
+    // updatating global variables value on mouse down event
+    isDragStart = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+const dragging = (e) => {
+    // scrolling images/carousel to left according to mouse pointer
+    if(!isDragStart) return;
+    e.preventDefault();
+    isDragging = true;
+    carousel.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons();
+}
+const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+    if(!isDragging) return;
+    isDragging = false;
+    autoSlide();
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+document.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("touchend", dragStop);
+</script>
+
+
+</body>
+
+
+
+
+</html>
